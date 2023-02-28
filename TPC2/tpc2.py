dddd42ca17164e93e480a0be44615a3240fd983d
danielfurtado11 <danielfurtado@gmail.com>
2023-02-28 00:22:13 +0000

    Trabalho Concluído

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,14 +1,34 @@
 valor = 1
 c = 0
 i = 0
+
+print("Insira dígitos para realizar uma soma:")
 while valor == 1:
     
-    x = input()
-    if (i == 1 and x != "=" and x.lower() != "off" and x.lower() != "on"):
-        c += int (x)
-    if (x == "="):
-        print(c)
-    if (x.lower() == "on"):
+    line = input(">> ")
+    numbers = ""
+
+    if line.lower() == "on":
         i = 1
-    if (x.lower() == "off"):
+    
+    if line.lower() == "off":
         i = 0
+
+    if line == "=":
+        print(c)
+        
+    if i == 1:
+        for caracter in line:
+            if caracter.isdigit():
+                numbers += caracter
+
+            else:
+                if numbers != "":
+                    c += int(numbers)
+                numbers = ""
+        if numbers != "":
+            c += int(numbers)
+            numbers = ""
+
+        
+    
